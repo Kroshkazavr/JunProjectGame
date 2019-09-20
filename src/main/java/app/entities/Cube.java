af357840a73b9e759576745eb185e0e5efdae098
Marsel Gazizov <marselgazizov84@gmail.com>
2019-09-20 22:58:45 +0300

    Classes

--- src/main/java/app/entities/Cube.java
+++ src/main/java/app/entities/Cube.java
@@ -1,4 +1,14 @@
 package app.entities;
 
 public class Cube {
-}
+        private int one = 1;
+        private int maxpoint = 9;
+        protected int value;
+
+        int createRandomNumber(String xxx) {    // метод при вводе xxx выводит значение от 1 до 9
+            System.out.println("Бросаю куб");
+            value = one + (int) (Math.random() * maxpoint);
+            return value;
+        }
+    }
+
--- src/main/java/app/entities/Person.java
+++ src/main/java/app/entities/Person.java
@@ -1,4 +1,27 @@
 package app.entities;
 
-public class Person {
-}
+public class Person extends Cube {
+        String name;
+        int victoriesCount;
+
+        protected int howMuchVictories() {
+            return victoriesCount;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", victoriesCount=" + victoriesCount +
+                    '}';
+        }
+    }
+
